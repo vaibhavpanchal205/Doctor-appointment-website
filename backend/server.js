06490c639fa31dd869a81be9c7418190b9893000
vaibhavpanchal205 <vaibhavpanchal205@gmail.com>
2025-01-02 23:24:44 +0530

    first

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+
+app.get('/', (req, res) => {
+    res.send('API WORKING ')
+})
+
+app.listen(port, () => console.log("Server started", port))
